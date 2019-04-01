f1cd1fa73b5ff1f1b7f8a0d72293c5233f27c9f4
Hoveff <hzs120203@126.com>
2019-04-01 09:16:18 +0800

    Initial Commit

---
+++ MyDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyDemo
+//
+//  Created by H&L on 2019/4/1.
+//  Copyright © 2019 hzsmac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyDemo
+//
+//  Created by H&L on 2019/4/1.
+//  Copyright © 2019 hzsmac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
